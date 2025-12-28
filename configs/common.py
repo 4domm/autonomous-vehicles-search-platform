7ca83dea06d80d778c62c6694aa0dcc15bd1831f
Venbiess <simpleonus5@mail.ru>
2025-12-28 15:15:11 +0000

    fix data_dir

--- configs/common.py
+++ configs/common.py
@@ -2,7 +2,7 @@ from .argoverse import *
 from .waymo import *
 from .nuscenes import *
 
-DATA_DIR = "app/data"
+DATA_DIR = "/app/data"
 
 # Available: WAYMO, ARGOVERSE
 # Make sure that the appropriate configs are filled in.
